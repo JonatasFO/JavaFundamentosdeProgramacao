3c97f6a289d5a9a4fd1e45385a4d3c1e3b629271
JonatasFO <jonatas_098@hotmail.com>
2018-09-17 22:51:28 -0300

    Atualizando...materia import

--- Curso Java/workspace/CursoJava/src/fundamentos/Comentario.java
+++ Curso Java/workspace/CursoJava/src/fundamentos/Comentario.java
@@ -31,4 +31,3 @@ public class Comentario {
 		System.out.println("Programa");
 	}
 }
-ddd
---
+++ Curso Java/workspace/CursoJava/src/fundamentos/Import.java
@@ -0,0 +1,22 @@
+package fundamentos;
+
+import java.awt.Button;
+import java.util.Date;
+
+/*
+ * Classe exemplificando o uso de "import"... As vezes precisamos declarar
+ * o import das classes que serão utilizadas 
+ */
+
+public class Import {
+	
+	public static void main(String[] args) {
+		
+		Double a = 2.0;
+		System.out.println(a);
+		
+		Button b;
+		Date c;
+	}
+
+}
